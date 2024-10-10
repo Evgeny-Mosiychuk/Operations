a8f26a11cb8b1b8dd7fcc7948d6f455669a95353
Evgeny Mosiychuk <Yevgeniy.Mosiychuk>
2024-10-10 14:24:42 +0200

    Remove unused imports

--- src/test/java/com/example/Paysonix/common/AbstractTest.java
+++ src/test/java/com/example/Paysonix/common/AbstractTest.java
@@ -2,11 +2,6 @@ package com.example.Paysonix.common;
 
 import com.example.Paysonix.PaysonixApplication;
 import com.example.Paysonix.service.SignatureService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,9 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,10 +23,6 @@ public class AbstractTest {
 
     @Value("${custom.token}")
     private String token;
-//    @Value("${custom.secret_key}")
-//    private String secretKey;
-//    @Value("${custom.algorithm}")
-//    private String algorithm;
     @Autowired
     protected MockMvc mvc;
     @Autowired
